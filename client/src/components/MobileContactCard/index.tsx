0423f286e7c6406fcbd612bed032025a1c9dfe4d
Aiden Gu <aiden.gu01@gmail.com>
2022-03-29 21:37:01 +1100

    Add hosting to firebase

--- client/src/components/MobileContactCard/index.tsx
+++ client/src/components/MobileContactCard/index.tsx
@@ -36,7 +36,7 @@ function MobileContactCard({
   contact,
 }: MobileContactCardProps) {
   return (
-    <div className="flex place-items-center bg-slate-800 flex-col mx-5 my-3 text-gray-500 p-5 hover:scale-105 transition ease-linear duration-100 group space-y-1">
+    <div className="flex place-items-center bg-gray-700 flex-col mx-5 my-3 text-gray-500 p-5 hover:scale-105 transition ease-linear duration-100 group space-y-1">
       <div className="flex flex-row items-center space-x-3">
         <div className="text-lg tracking-wider group-hover:text-sky-400 transition duration-100 ease-linear text-white">{contact.name}</div>
       </div>
